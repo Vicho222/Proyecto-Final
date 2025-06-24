429bf2e40a750b53e9f7227ff13d6009908a8c8e
Vicente Antonio Martínez Moris <vicemartinez@alumnos.uai.cl>
2025-06-24 10:57:09 -0400

    Update HexBoardFactory.java Listo

--- src/main/java/com/atraparalagato/impl/factories/HexBoardFactory.java
+++ src/main/java/com/atraparalagato/impl/factories/HexBoardFactory.java
@@ -1,1 +1,20 @@
+package com.atraparalagato.impl.factories;
 
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
+
+import com.atraparalagato.base.model.GameBoard;
+import com.atraparalagato.impl.model.HexGameBoard;
+import com.atraparalagato.impl.model.HexPosition;
+
+@Component
+public class HexBoardFactory implements Function<Integer, GameBoard<HexPosition>>{
+
+	@Override
+	public GameBoard<HexPosition> apply(Integer size) {
+		// TODO Auto-generated method stub
+		return new HexGameBoard(size);
+	}
+    
+}
