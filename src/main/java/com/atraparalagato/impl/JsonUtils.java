5bf4022d4631f9d20e6fdf6bb7c8981f62d7f8e4
Vicente Antonio Martínez Moris <vicemartinez@alumnos.uai.cl>
2025-06-24 10:48:21 -0400

    Update JsonUtils.java

--- src/main/java/com/atraparalagato/impl/JsonUtils.java
+++ src/main/java/com/atraparalagato/impl/JsonUtils.java
@@ -25,3 +25,59 @@ public class JsonUtils {
         }
     }
 }
+package com.atraparalagato.impl;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+public class JsonUtils {
+	
+	
+	
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	public static final ObjectMapper MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule()) // para soporte de fechas Java 8
+            .registerModule(customTimeModule()) // para aplicar el formato
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+    private static SimpleModule customTimeModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(FORMATTER));
+        return module;
+    }
+    
+    public static <T> T fromJson(String json, TypeReference<T> typeRef) {
+        try {
+            return MAPPER.readValue(json, typeRef);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return MAPPER.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String toJson(Object obj) {
+        try {
+            return MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
