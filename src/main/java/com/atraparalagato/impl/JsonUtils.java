33f08fabfe7dff7157f529b574eaf8e8be6bb0cb
Vicente Antonio Martínez Moris <vicemartinez@alumnos.uai.cl>
2025-06-24 10:48:50 -0400

    Json utils listo

--- src/main/java/com/atraparalagato/impl/JsonUtils.java
+++ src/main/java/com/atraparalagato/impl/JsonUtils.java
@@ -1,33 +1,6 @@
 package com.atraparalagato.impl;
 
 import java.io.IOException;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class JsonUtils {
-	
-	public static final ObjectMapper MAPPER = new ObjectMapper();
-
-    public static <T> T fromJson(String json, Class<T> clazz) {
-        try {
-            return MAPPER.readValue(json, clazz);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String toJson(Object obj) {
-        try {
-            return MAPPER.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-package com.atraparalagato.impl;
-
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
